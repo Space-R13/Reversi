fb7d67fc8d499b13dee9148a5960973d84e44e50
andrey1303@mail.ru <Githab1303>
2018-03-29 16:10:20 +0300

    Window

--- src/com/company/WindowBasic.java
+++ src/com/company/WindowBasic.java
@@ -5,22 +5,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class WindowBasic extends Frame{
 
 
-    private WindowBasic() {
-   /**     JFrame windowB = new JFrame("Реверси");
-        windowB.setSize(500,500);
-        windowB.setVisible(true);
-        windowB.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        windowB.setResizable(false);
-        JButton[][] buttonG = new JButton[x][y];
-        for ( x = 0; x < 8; x++ ) {
-            for ( y = 0; y < 8; y++) {
-                windowB.add(buttonG[x][y]);
-            }
-        }*/
+    public WindowBasic() {
+
         JFrame frame = new JFrame("Reverse");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         winBas(frame.getContentPane());
@@ -30,74 +22,56 @@ public class WindowBasic extends Frame{
         frame.setResizable(false);
     }
 
-    private void winBas(Container container){
-        int x , y;
-        JButton button = new JButton();
+    private void winBas(Container container) {
+        int x, y;
+        JPanel panel;
+        JButton res;
+        JButton clo;
         JTextField player, score;
         container.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         container.setLayout(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
-        //constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.fill = GridBagConstraints.VERTICAL;
-        constraints.weightx = 0;
+    //    constraints.weightx = 0.5;
         constraints.gridy = 0;
-
-
-
-        button = new JButton("Reset");
-        //constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.fill = GridBagConstraints.VERTICAL;
         constraints.gridx = 0;
-        constraints.gridwidth = 2;
-        constraints.gridy = 0;
-        container.add(button, constraints);
 
-        score = new JTextField(3);
-        //constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.fill = GridBagConstraints.VERTICAL;
+        panel = new JPanel();
+        res = new JButton("Reset");
+        panel.add(res);
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        container.add(panel, constraints);
+
+        score = new JTextField(5);
+        panel.add(score);
         constraints.gridx = 1;
-       constraints.gridwidth = 2;
         constraints.gridy = 0;
         container.add(score, constraints);
 
-        player = new JTextField(3);
-      //  constraints.fill = GridBagConstraints.HORIZONTAL;
-       constraints.fill = GridBagConstraints.VERTICAL;
+        player = new JTextField(5);
+        panel.add(player);
         constraints.gridx = 2;
-       constraints.gridwidth = 2;
         constraints.gridy = 0;
         container.add(player, constraints);
 
-        button = new JButton("Close");
-    //    constraints.fill = GridBagConstraints.HORIZONTAL;
-      constraints.fill = GridBagConstraints.VERTICAL;
+        clo = new JButton("Close");
+        panel.add(clo);
         constraints.gridx = 3;
-        constraints.gridwidth = 2;
         constraints.gridy = 0;
-        container.add(button, constraints);
+        container.add(clo, constraints);
 
         JButton[][] buttonG = new JButton[8][8];
-        //constraints.weightx = 0;
-        constraints.fill = GridBagConstraints.VERTICAL;
-        constraints.gridwidth = 1;
-        constraints.gridx = 0;    // нулевая ячейка по горизонтали
-        constraints.gridy = 1;    // первая ячейка по вертикали
-
-        container.setLayout(new GridLayout(8,8));
-
-        for ( x = 0; x < 8; x++ ) {
-            for ( y = 0; y < 8; y++) {
+        panel = new JPanel(new GridLayout(8,8));
+        for (x = 0; x < 8; x++) {
+            for (y = 0; y < 8; y++) {
                 buttonG[x][y] = new JButton();
                 buttonG[x][y].addActionListener(new PositionAwareActionListener(x, y));
-                container.add(buttonG[x][y], constraints);
+                panel.add(buttonG[x][y]);
             }
         }
-
-
-
-
-
+        constraints.weightx = 0.0;
+        constraints.gridx = 0;    // нулевая ячейка по горизонтали
+        constraints.gridy = 1;    // первая ячейка по вертикали
+        container.add(panel, constraints);
     }
-
-
 }
