c8864236ce6125e688d9cd0cccd254e82443e965
andrey1303@mail.ru <Githab1303>
2018-03-30 09:02:42 +0300

    Window

--- src/com/company/Chip.java
+++ src/com/company/Chip.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+
 public class Chip {
-    Chip(){}
+    Chip(){
+    }
 }
+
